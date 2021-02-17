8fb307685d4e1cd5a98eb08c0dbdde706c486da4
Paul Ballas <paballas51@gmail.com>
2021-02-16 22:02:30 -0500

    making prog

--- index.js
+++ index.js
@@ -2,28 +2,35 @@ const [_, __, city] = process.argv;
 const census = require("citysdk");
 const axios = require("axios");
 
-const citySearch = async (city) => {
+const censusPromise = async (county) => {
+  return new Promise((resolve, reject) => {
+    census(
+      {
+        vintage: 2019, // required
+        geoHierarchy: {
+          county: {
+            lat: 26.124161,
+            lng: -80.143593
+          }
+        }
+      },
+      (err, data) => {
+        if (err) reject(err);
+        else resolve(data);
+      }
+    );
+  });
+};
+
+const citySearch = async (county) => {
+  const convertCounty;
   const getData = await axios({
     method: "GET",
     url: `https://api.census.gov/data/2019/pep/housing?get=LASTUPDATE,HUEST,DATE_DESC&for=county:001&in=state:01`
   });
 
-  console.log(city);
+  console.log(county);
   console.log(getData.data);
-  //   census(
-  //     {
-  //       vintage: 2019, // required
-  //       geoHierarchy: {
-  //         county: {
-  //           lat: 26.124161,
-  //           lng: -80.143593
-  //         },
-  //         sourcePath: ["pep", "housing"],
-  //         values: ["HUEST"]
-  //       }
-  //     },
-  //     (err, res) => console.log("debug", res)
-  //   );
 };
 
 citySearch(city);
